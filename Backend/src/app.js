cff81ff3c83c151fa733894b0d395693d57ba6a7
Geetanjali17Nayak <117501359+Geetanjali17Nayak@users.noreply.github.com>
2025-09-21 11:32:30 +0530

    feat: added port

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,8 @@ export const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000", // The origin of your frontend app
+  // origin: "http://localhost:3000",
+   origin: "https://nestfinder-frontend.onrender.com", // The origin of your frontend app
   credentials: true,
 }));
 app.use("/api/users", userRouter);
